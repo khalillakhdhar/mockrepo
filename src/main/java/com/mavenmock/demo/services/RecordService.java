de6c20d9c4bf567cf34112cf3195ee99a8958a73
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-25 05:19:20 +0100

    Update RecordService.java

--- src/main/java/com/mavenmock/demo/services/RecordService.java
+++ src/main/java/com/mavenmock/demo/services/RecordService.java
@@ -1,5 +1,59 @@
 package com.mavenmock.demo.services;
 
-public class RecordService {
+import com.mavenmock.demo.dao.DatabaseDAO;
+import com.mavenmock.demo.dao.NetworkDAO;
 
+public class RecordService 
+{
+    DatabaseDAO database;
+    NetworkDAO network;
+     
+    
+    
+    
+    
+    //setters and getters
+ 
+    public DatabaseDAO getDatabase() {
+		return database;
+	}
+
+
+
+
+
+	public void setDatabase(DatabaseDAO database) {
+		this.database = database;
+	}
+
+
+
+
+
+	public NetworkDAO getNetwork() {
+		return network;
+	}
+
+
+
+
+
+	public void setNetwork(NetworkDAO network) {
+		this.network = network;
+	}
+
+
+
+
+
+	public boolean save(String fileName) 
+    {
+        database.save(fileName);
+        System.out.println("Saved in database in Main class");
+         
+        network.save(fileName);
+        System.out.println("Saved in network in Main class");
+         
+        return true;
+    }
 }
